80d81785fb63de896e21f3dd72d325a6f4099a9e
Robert Allen <zircote@gmail.com>
2011-11-01 01:24:53 -0500

    sencha development and setup of gaming app for html5 devices

--- public/index.php
+++ public/index.php
@@ -2,11 +2,14 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define(
+        'APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application')
+       );
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? 
+            getenv('APPLICATION_ENV') : 'production'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
@@ -15,12 +18,25 @@ set_include_path(implode(PATH_SEPARATOR, array(
 )));
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
-
+require_once 'Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance();
 // Create application, bootstrap, and run
+$application = new Zend_Config_Ini(
+    APPLICATION_PATH . '/configs/application.ini',
+    APPLICATION_ENV, 
+    array('allowModifications' => true)
+);
+/* Define Routes and merge */
+$routes = new Zend_Config_Ini(
+    APPLICATION_PATH . '/configs/routes.ini',
+    APPLICATION_ENV, 
+    array('allowModifications' => true)
+);
+$application->merge($routes);
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+    $application
 );
+
 $application->bootstrap()
             ->run();
